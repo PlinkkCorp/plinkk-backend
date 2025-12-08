4fb095eaaab100b2f021b7b7a039fdd937273e20
Klaynight-dev <night.psr@gmail.com>
2025-12-08 14:04:28 +0100

    debug: list generated files and force index import

--- packages/prisma/index.d.ts
+++ packages/prisma/index.d.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "./generated/prisma/index";
 import { PrismaPg } from "@prisma/adapter-pg";
-export * from "./generated/prisma";
+export * from "./generated/prisma/index";
 export declare const prisma: PrismaClient<{
     adapter: PrismaPg;
 }, never, import("./generated/prisma/runtime/library").DefaultArgs>;
