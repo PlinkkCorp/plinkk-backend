178ec49d9ad32213e928b809080bac70cf3f8b66
Klaynight-dev <night.psr@gmail.com>
2025-12-08 22:44:00 +0100

    fix: switch back to postgresql adapter

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -1,46 +1,11 @@
 import { PrismaClient } from "./generated/prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-// import { PrismaPg } from "@prisma/adapter-pg"
-import path from "path";
-import fs from "fs";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 export * from "./generated/prisma/client";
 
+const connectionString = `${process.env.DATABASE_URL}`;
 
-if (!process.env.DATABASE_URL || process.env.DATABASE_URL.startsWith("file:")) {
-    let dbAbsolute;
-    // Try to find workspace root to locate the central database
-    let currentDir = __dirname;
-    let workspaceRoot = null;
-    while (currentDir !== path.parse(currentDir).root) {
-        if (fs.existsSync(path.join(currentDir, "pnpm-workspace.yaml"))) {
-            workspaceRoot = currentDir;
-            break;
-        }
-        currentDir = path.dirname(currentDir);
-    }
-
-    if (workspaceRoot) {
-        dbAbsolute = path.join(workspaceRoot, "packages", "prisma", "prisma", "dev.db");
-    } else {
-        const isDist = __dirname.endsWith("dist");
-        dbAbsolute = isDist
-            ? path.resolve(__dirname, "..", "prisma", "dev.db")
-            : path.resolve(__dirname, "prisma", "dev.db");
-    }
-
-    console.log("Prisma DB Path:", dbAbsolute);
-	const dbDir = path.dirname(dbAbsolute);
-	if (!fs.existsSync(dbDir)) {
-		fs.mkdirSync(dbDir, { recursive: true });
-	}
-	// if (!fs.existsSync(dbAbsolute)) {
-	// 	fs.writeFileSync(dbAbsolute, "");
-	// }
-	process.env.DATABASE_URL = `file:${dbAbsolute}`;
-}
-
-const connectionString = `${process.env.DATABASE_URL}`
-
-const adapter = new PrismaLibSql({ url: connectionString });
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
